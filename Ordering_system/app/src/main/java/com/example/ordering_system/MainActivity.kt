69c816185540d7ff28786f3416f9cefceac88ed4
che <a0965494713@gmail.com>
2025-12-18 17:45:11 +0800

    ordering_Lab_finish

--- Ordering_system/app/src/main/java/com/example/ordering_system/MainActivity.kt
+++ Ordering_system/app/src/main/java/com/example/ordering_system/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.ordering_system
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnChoice = findViewById<Button>(R.id.button)
+        btnChoice.setOnClickListener {
+
+            val intent = Intent(this, activity_sec::class.java)
+
+            startForResult.launch(intent)
+        }
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {
+        result: ActivityResult ->
+        if(result.resultCode == Activity.RESULT_OK)
+        {
+            val intent = result.data
+            val drink = intent?.getStringExtra("drink")
+            val sugar = intent?.getStringExtra("sugar")
+            val ice = intent?.getStringExtra("ice")
+            val tvMeal = findViewById<TextView>(R.id.Meal)
+            tvMeal.text ="飲料:$drink\n\n甜度:$sugar\n\n冰塊:$ice"
         }
     }
 }
--- Ordering_system/app/src/main/java/com/example/ordering_system/activity_sec.kt
+++ Ordering_system/app/src/main/java/com/example/ordering_system/activity_sec.kt
@@ -1,8 +1,15 @@
 package com.example.ordering_system
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +22,33 @@ class activity_sec : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val beverage = findViewById<TextView>(R.id.input)
+        val sugar = findViewById<RadioGroup>(R.id.RadioGroup)
+        val ice = findViewById<RadioGroup>(R.id.RadioGroup2)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+
+            if (beverage.text.isEmpty()) {
+                Toast.makeText(this, "請輸入飲料名稱", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val b = bundleOf(
+                    "drink" to beverage.text.toString(),
+                    "sugar" to sugar.findViewById<RadioButton>(
+                        sugar.checkedRadioButtonId
+                    ).text.toString(),
+                    "ice" to ice.findViewById<RadioButton>(
+                        ice.checkedRadioButtonId
+                    ).text.toString()
+                )
+
+                val i = Intent().putExtras(b)
+
+                setResult(RESULT_OK, i)
+                finish()
+            }
         }
     }
 }
